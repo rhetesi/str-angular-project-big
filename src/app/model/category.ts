6a09004d6a191e67ec416e4d4bf7eb5909e5976a
Monika <mon.fur89@gmail.com>
2021-02-23 07:53:16 +0100

    new classes added

---
+++ src/app/model/category.ts
@@ -0,0 +1,15 @@
+export class Category {
+    id: number = 0;
+    name: string = '';
+    description: string = '';
+
+    constructor(properties?: Category) {
+        if (properties) {
+            this.id = properties.id || 0;
+            this.name = properties.name || '';
+            this.description = properties.description || '';
+        }
+    }
+}
+
+//Category (id, name, description)
