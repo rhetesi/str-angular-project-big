9bc578d1bbbdb9eadb227ca99c2152a0b9009cba
Luis <skorpio19691102@freemail.hu>
2021-02-23 11:28:26 +0100

    bill service ts

--- src/app/service/bill.service.ts
+++ src/app/service/bill.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { bil } from '../model/bill';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BillService {
+export class CustomerService {
 
-  constructor() { }
+  jsonUrl: string = 'http://localhost:3000/bill';
+
+  list$: BehaviorSubject<bil[]> =
+    new BehaviorSubject<bil[]>([]);
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): void {
+    this.http.get<bil[]>(this.jsonUrl).subscribe(
+      bils => this.list$.next(bils));
+  }
+
+  get(id: number | string): Observable<bil> {
+    id = parseInt('' + id);
+    return this.http.get<bil>(`${this.jsonUrl}/${id}`);
+  }
+
+  create(bil: bil): void {
+    this.http.post<bil>(`${this.jsonUrl}`, bil
+    ).subscribe(() => this.getAll());
+  }
+
+  update(bil: bil): Observable<bil> {
+    return this.http.patch<bil>(`${this.jsonUrl}/${bil.id}`, bil
+    ).pipe(
+      tap(() => this.getAll())
+    );
+  }
+
+  remove(bil: bil): void {
+    this.http.delete(`${this.jsonUrl}/${bil.id}`
+    ).subscribe(() => this.getAll());
+  }
 }
