d72de23b8b9ce4e9ced63a7b57b3cd50eeb61122
Luis <skorpio19691102@freemail.hu>
2021-02-23 11:46:16 +0100

    Bill

--- src/app/service/bill.service.ts
+++ src/app/service/bill.service.ts
@@ -2,44 +2,44 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { bill } from '../model/bill';
+import { Bill } from '../model/bill';
 
 @Injectable({
   providedIn: 'root'
 })
-export class billService {
+export class BillService {
 
-  jsonUrl: string = 'http://localhost:3000/bill';
+  jsonUrl: string = 'http://localhost:3000/bills';
 
-  list$: BehaviorSubject<bil[]> =
-    new BehaviorSubject<bil[]>([]);
+  list$: BehaviorSubject<Bill[]> =
+    new BehaviorSubject<Bill[]>([]);
 
   constructor(private http: HttpClient) { }
 
   getAll(): void {
-    this.http.get<bil[]>(this.jsonUrl).subscribe(
+    this.http.get<Bill[]>(this.jsonUrl).subscribe(
       bils => this.list$.next(bils));
   }
 
-  get(id: number | string): Observable<bil> {
+  get(id: number | string): Observable<Bill> {
     id = parseInt('' + id);
-    return this.http.get<bil>(`${this.jsonUrl}/${id}`);
+    return this.http.get<Bill>(`${this.jsonUrl}/${id}`);
   }
 
-  create(bil: bil): void {
-    this.http.post<bil>(`${this.jsonUrl}`, bil
+  create(bill: Bill): void {
+    this.http.post<Bill>(`${this.jsonUrl}`, bil
     ).subscribe(() => this.getAll());
   }
 
-  update(bil: bil): Observable<bil> {
-    return this.http.patch<bil>(`${this.jsonUrl}/${bil.id}`, bil
+  update(bill: Bill): Observable<Bill> {
+    return this.http.patch<Bill>(`${this.jsonUrl}/${bill.id}`, bil
     ).pipe(
       tap(() => this.getAll())
     );
   }
 
-  remove(bil: bil): void {
-    this.http.delete(`${this.jsonUrl}/${bil.id}`
+  remove(bill: Bill): void {
+    this.http.delete(`${this.jsonUrl}/${bill.id}`
     ).subscribe(() => this.getAll());
   }
 }
