7946d3779bc8d4fcd861ce855d66f477ab22c1c4
Luis <skorpio19691102@freemail.hu>
2021-02-24 20:49:40 +0100

    List-bill

--- src/app/pages/list-bill/list-bill.component.ts
+++ src/app/pages/list-bill/list-bill.component.ts
@@ -4,25 +4,25 @@ import { Bill } from 'src/app/model/bill';
 import { BillService } from 'src/app/service/bill.service';
 
 @Component({
-  selector: 'app-list-order',
+  selector: 'app-list-bill',
   templateUrl: './list-bill.component.html',
   styleUrls: ['./list-bill.component.scss']
 })
 export class ListBillComponent implements OnInit {
 
-  orderList$: BehaviorSubject<Bill[]> = this.orderService.list$;
+  billList$: BehaviorSubject<Bill[]> = this.billService.list$;
   testBill: Observable<Bill> = this.billService.get(1);
   billService: any;
 
   constructor(
-    private orderService: BillService,
+    private billService: BillService,
   ) { }
 
   ngOnInit(): void {
-    this.orderService.getAll();
+    this.billService.getAll();
   }
 
-  onDelete(order: Bill) {
+  onDelete(bill: Bill) {
     this.billService.delete(Bill);
   }
 }
