e224d7488b05f5c603afaaf6992402b0245dbf6b
Luis <skorpio19691102@freemail.hu>
2021-02-24 19:59:41 +0100

    list-bill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ListBillComponent } from './pages/list-bill/list-bill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListBillComponent
   ],
   imports: [
     BrowserModule,
