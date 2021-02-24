03bfd81921391045aefc2e0d479a6a1f539bc68b
Piktorikusz <piktorikusz@gmail.com>
2021-02-24 22:34:23 +0100

    new ts

--- src/app/pages/list-product/list-product.component.ts
+++ src/app/pages/list-product/list-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-list-product',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductComponent implements OnInit {
 
-  constructor() { }
+  productList$: BehaviorSubject<Product[]> =
+  this.productService.list$;
+
+  phraseControl: FormControl = new FormControl('');
+  phrase: string = '';
+
+  constructor(
+    private productService: ProductService,
+  ) { }
 
   ngOnInit(): void {
+    this.productService.getAll();
+    this.phraseControl.valueChanges.pipe(
+      debounceTime(800)
+    ).subscribe(
+      newValue => this.phrase = newValue
+    );
+  }
+
+  onDelete(product: Product) {
+    this.productService.remove(product);
   }
 
 }
