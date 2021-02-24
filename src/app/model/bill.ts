c8447dbba3aa614ac269bccea5e1acefcf7cf216
Luis <skorpio19691102@freemail.hu>
2021-02-24 22:22:43 +0100

    Edit ID

--- src/app/model/bill.ts
+++ src/app/model/bill.ts
@@ -1,17 +1,17 @@
 export class Bill {
     id: number = 0;
-    orderId: number = 0;
+    orderID: number = 0;
     amount: number = 0;
     status: "new" | "paid" = "new";
-    
+
 
     constructor(properties?: Bill) {
         if (properties) {
             this.id = properties.id || 0;
-            this.orderId = properties.orderId || 0;
+            this.orderID = properties.orderID || 0;
             this.amount = properties.amount || 0;
         }
     }
 }
 
-//Bill (id, orderID, amount, status:new|paid)  
+//Bill (id, orderID, amount, status:new|paid)
