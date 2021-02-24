4170dfd1c140484720b0be83326c660ce0340a0b
Monika <mon.fur89@gmail.com>
2021-02-24 21:21:38 +0100

    navbar, sidebar, dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { DashboardComponent } from './common/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
