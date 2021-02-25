772842eb074b48463b04fb2322834cd85fbf5ee8
rhetesi <rhetesi@gmail.com>
2021-02-25 21:11:18 +0100

    Add: routing, modified sidebar & toaster && test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './common/dashboard/dashboard.component';
+import { EditOrderComponent } from './editor/edit-order/edit-order.component';
+import { ListBillComponent } from './pages/list-bill/list-bill.component';
+import { ListCustomerComponent } from './pages/list-customer/list-customer.component';
 import { ListOrderComponent } from './pages/list-order/list-order.component';
+import { ListProductComponent } from './pages/list-product/list-product.component';
 
 const routes: Routes = [
   {
    path: '',
    component: DashboardComponent,
   },
-  
+  {
+   path: 'customers',
+   component: ListCustomerComponent,
+  },
+  {
+   path: 'products',
+   component: ListProductComponent,
+  },
   {
     path: 'orders',
     component: ListOrderComponent,
   },
-  // {
-  //   path: 'orders/:id',
-  //   component: EditOrderComponent,
-  // },
+  {
+    path: 'orders/:id',
+    component: EditOrderComponent,
+  },
+  {
+   path: 'bills',
+   component: ListBillComponent,
+  },
   {
     path: '**',
     redirectTo: '',
--- src/app/editor/edit-order/edit-order.component.ts
+++ src/app/editor/edit-order/edit-order.component.ts
@@ -71,7 +71,7 @@ export class EditOrderComponent implements OnInit {
         </tbody>
       </table>`,
       title,
-      5000)
+      10000)
   }
 
 }
