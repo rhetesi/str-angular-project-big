f08c1056a6bbb5c41e4e798d6d79b828c35eb38b
Piktorikusz <piktorikusz@gmail.com>
2021-02-26 21:29:11 +0100

    editor alap kész

--- src/app/editor/edit-product/edit-product.component.ts
+++ src/app/editor/edit-product/edit-product.component.ts
@@ -4,8 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { Product } from 'src/app/model/product';
-import { ProductService } from 'src/app/service/product.service';
 import { NotificationService } from 'src/app/service/notification.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-edit-product',
@@ -15,13 +15,12 @@ import { NotificationService } from 'src/app/service/notification.service';
 export class EditProductComponent implements OnInit {
   
   product$: Observable<Product> = this.activatedRoute.params.pipe(
-      switchMap( params => this.productService.get (params.id))
+    switchMap( params => this.productService.get(params.id))
   );
-  
-  clicked: boolean = false;
-  category: string = 'new';
-  
 
+
+  clicked: boolean = false;
+  
   constructor(
     private activatedRoute: ActivatedRoute,
     private productService: ProductService,
@@ -32,88 +31,50 @@ export class EditProductComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  //Url ből idt 
-  //ProductService.get et meghívni
-  
-  //új eseményhez params.id === 0
-  
   onUpdate(form: NgForm, product: Product): void {
-      this.productService.update(product).subscribe(
-      ev => this.router.navigate([''])
-      );
-      console.log(form.value, product);
-  }
-
-  onCreate(form: NgForm, product: Product): void {
-      this.productService.create(product)
-  }
-  showHtmlToaster(product: Product) {
-    let title: string = "You have updated this Product:";
+    this.clicked = true;
     if (!product.id) {
-      title = "You have added this new Product:";
+      this.productService.create(product);
+      this.router.navigate(['products']);
+      } else {
+        this.productService.update(product).subscribe(
+          () => this.router.navigate(['products'])
+        );
+      }
+      console.log('onUpdate:',form.value, product)
     }
-    this.notifyService.showSuccessWithTimeout(`
+
+  showHtmlToaster(product: Product) {
+    let title: string = "You have updated this product:";
+    if (!product.id) {
+      title = "You have added this new product:";
+  }
+  this.notifyService.showSuccessWithTimeout(`
       <br>
       <br>
       <table class="table">
         <thead>
           <tr>
             <th>catID</th>
-            <th>name</th>
-            <th>description</th>
-            <th>price</th>
+            <th>product name</th>
+            <th>product type</th>
+            <th>product description</th>
+            <th>product price</th>
             <th>featured</th>
-            <th>active</th>
           </tr>
         </thead>
         <tbody>
           <tr class="text-success">
             <td>${product.catID}</td>
-            <td>${product.name}</td>
+            <td>${product.name} </td>
+            <td>${product.type}</td>
             <td>${product.description}</td>
             <td>${product.price}</td>
             <td>${product.featured}</td>
-            <td>${product.active}</td>
           </tr>
         </tbody>
       </table>`,
       title,
       10000)
-  }
-
+    }
 }
-
-// servicbe
-
-// get(id: number): Observable<Event> {
-//  id = parseInt( '' + id ), 10) vagy
-// if (typeof id === 'string') {
-//      id = parseInt(id, 10);
-// }
-
-//vagy    id = typof id ==='string' ? parseInt(id, 10) : id;
-//  const ev: Product | undefined = this.list.find( item => item.id === id );
-//  if (ev) {
-//    return of(ev);
-//  }
-//  
-//  return of(new Product());
-//
-//}
-
-//update(product: Porduct) :Observable<Product> {
-//  const index: number = this.list.findIndex( item ===> product.id );
-//  this.list.splice(index, 1 , product);
-//  this.getAll();
-//  return of (this.list[index]);    Http kérés  http.get
-//}
-
-//id: number = 0;
-//catID: number = 0;
-//name: string = '';
-//type: string = '';
-//description: string = '';
-//price: number = 0;
-//featured: boolean = false;
-//active: boolean = true;
-//image?: string = '';
