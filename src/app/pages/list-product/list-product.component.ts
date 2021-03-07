de1c63d0ade58dda8d3376bcaeb2caf5f91f11fa
Piktorikusz <piktorikusz@gmail.com>
2021-03-07 17:01:30 +0100

    format

--- src/app/pages/list-product/list-product.component.ts
+++ src/app/pages/list-product/list-product.component.ts
@@ -69,49 +69,49 @@ export class ListProductComponent implements OnInit {
     $.notify({
       icon: "notifications",
       message: title
-  },
-    {
-      type: type[color],
-      timer: 4000,
-      placement: {
-          from: from,
-          align: align
-      },
-      template: `<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">
-        <button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>
-        <i class="material-icons" data-notify="icon">notifications</i>
-        <span data-notify="title">{1}</span>
-        <span data-notify="message">{2}</span>
-        <div class="progress" data-notify="progressbar">
-          <div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
-        </div>
-        <a href="{3}" target="{4}" data-notify="url"></a>
-        <br>
-        <table class="table">
-          <thead>
-            <tr>
-              <th>productID</th>
-              <th>catID</th>
-              <th>name</th>
-              <th>description</td>
-              <th>price</th>
-              <th>featured</th>
-              <th>active</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>${product.id}</td>
-              <td>${product.catID} </td>
-              <td>${product.name}</td>
-              <td>${product.description}</td>
-              <td>${product.price}</td>
-              <td>${product.featured}</td>
-              <td>${product.active}</td>
-            </tr>
-          </tbody>
-        </table>
-        </div>`
+    },
+      {
+        type: type[color],
+        timer: 4000,
+        placement: {
+            from: from,
+            align: align
+        },
+        template: `<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">
+          <button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>
+          <i class="material-icons" data-notify="icon">notifications</i>
+          <span data-notify="title">{1}</span>
+          <span data-notify="message">{2}</span>
+          <div class="progress" data-notify="progressbar">
+            <div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>
+          </div>
+          <a href="{3}" target="{4}" data-notify="url"></a>
+          <br>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>productID</th>
+                <th>catID</th>
+                <th>name</th>
+                <th>description</td>
+                <th>price</th>
+                <th>featured</th>
+                <th>active</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>${product.id}</td>
+                <td>${product.catID} </td>
+                <td>${product.name}</td>
+                <td>${product.description}</td>
+                <td>${product.price}</td>
+                <td>${product.featured}</td>
+                <td>${product.active}</td>
+              </tr>
+            </tbody>
+          </table>
+          </div>`
     });
   }
 
